6492c8326140fea7a20a425c046f96ff0530131b
Nicole J <nicole.a.jordaan@gmail.com>
2023-10-05 15:28:52 +0100

    Added route to retrieve static image file

--- hello.py
+++ hello.py
@@ -5,6 +5,13 @@ app = Flask(__name__)
 def root():
     return "The default, 'root' route"
 
+@app.route('/static-example/img')
+def static_example_img():
+    start = '<img src="'
+    url = url_for('static', filename='vmask.jpg')
+    end = '">'
+    return start+url+end, 200
+
 @app.route("/hello/")
 def hello_world():
     return "Hello World"

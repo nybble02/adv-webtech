6d03031e58be0013335c46df1c6a573d8b5809f1
Nicole J <nicole.a.jordaan@gmail.com>
2023-10-05 13:13:50 +0100

    Added routes and redirects

--- hello.py
+++ hello.py
@@ -1,6 +1,43 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, abort
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
+def root():
+    return "The default, 'root' route"
+
+@app.route("/hello/")
 def hello_world():
-    return 'Hello World'
+    return "Hello World"
+
+@app.route("/napier/")
+def hello_napier    ():
+    return "Hello Napier!!! :D"
+
+@app.route("/goodbye/")
+def goodbye():
+    return "Goodbye cruel world :("
+
+@app.route('/login')
+def login():
+    return "Now we would get username & passowrd"
+
+@app.route("/private")
+def private():
+    # Test for user logged in failed
+    # so redirect to login url
+    return redirect(url_for('login'))
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return "Couldn't find the page you requested", 404
+
+@app.route('/force404')
+def force404():
+    abort(404)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
+
+
+
+
